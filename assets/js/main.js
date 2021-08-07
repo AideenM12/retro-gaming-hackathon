4273920fb1dda616539e1551a3ff434f9d07ae84
Anthony O'Brien <aux.fuse@yahoo.com>
2021-08-07 22:35:11 +0100

    New audio file for door close and extra physics platforms

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,18 +46,22 @@ function create () {
     floor = platforms.create(0, ((window.innerHeight * 1) - 48) ,'floor').setScale(3).setOrigin(0, 0).refreshBody();
     floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.7) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
     floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.8) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
+    floor4 = platforms.create(((floor.width * 3) + (floor3.width * 6)), (window.innerHeight * 0.5), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor5 = platforms.create(((floor.width * 3.5) + (floor4.width)), (window.innerHeight * 0.2), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
 
     // static scene objects doors/skulls/ladders etc
     door = this.add.image(50, (window.innerHeight * 0.65), 'door').setScale(3).setOrigin(0, 0);
     doorCloseAudio = this.sound.play('door-close');
     singleFloorTile = this.add.image(50, (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
     singleFloorTile2 = this.add.image((50 + (singleFloorTile.width * 4)), (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
+
     // using crate image as pseudo-ladder image
     ladder = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.71)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
     ladder2 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.75)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
     ladder3 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.79)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
     ladder4 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.83)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
     ladder5 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.87)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+
     // ambient background objects
     blueFlaskBig = this.add.image(40, (window.innerHeight * 0.92), 'blue-Flask-Big').setScale(2).setOrigin(0, 0);
     skull = this.add.image(40, (window.innerHeight * 0.9), 'skull').setScale(4).setOrigin(0, 0);
