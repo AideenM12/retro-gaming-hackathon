cb32815999452ad6e75f4af3095174d5743a6436
Anthony O'Brien <aux.fuse@yahoo.com>
2021-08-07 22:20:17 +0100

    Small graphic details added to level with door/ladder and door audio

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,14 @@ let game = new Phaser.Game(config);
 let playerSprite = 'assets/tileset/frames/playerSprite/wiz-m-spritesheet.png';
 
 function preload () {
-    this.load.image('floor', 'assets/tileset/frames/environment/floor/floor-group.png')
+    this.load.image('floor', 'assets/tileset/frames/environment/floor/floor-group.png');
+    this.load.image('single-floor', 'assets/tileset/frames/environment/floor/floor_5.png');
+    this.load.image('door', 'assets/tileset/frames/environment/door/doors_leaf_closed.png');
+    this.load.image('crate', 'assets/tileset/frames/environment/objects/crate.png');
+    this.load.image('skull', 'assets/tileset/frames/enemies/skull.png');
+    this.load.image('blue-Flask-Big', 'assets/tileset/frames/environment/objects/flask_big_blue.png');
+
+    this.load.audio('door-close', 'assets/audio/door-creek-slam.wav');
 
     this.load.spritesheet(
         'player',
@@ -40,8 +47,24 @@ function create () {
     floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.7) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
     floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.8) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
 
+    // static scene objects doors/skulls/ladders etc
+    door = this.add.image(50, (window.innerHeight * 0.65), 'door').setScale(3).setOrigin(0, 0);
+    doorCloseAudio = this.sound.play('door-close');
+    singleFloorTile = this.add.image(50, (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
+    singleFloorTile2 = this.add.image((50 + (singleFloorTile.width * 4)), (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
+    // using crate image as pseudo-ladder image
+    ladder = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.71)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+    ladder2 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.75)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+    ladder3 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.79)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+    ladder4 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.83)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+    ladder5 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.87)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
+    // ambient background objects
+    blueFlaskBig = this.add.image(40, (window.innerHeight * 0.92), 'blue-Flask-Big').setScale(2).setOrigin(0, 0);
+    skull = this.add.image(40, (window.innerHeight * 0.9), 'skull').setScale(4).setOrigin(0, 0);
+
     // character
-    player = this.physics.add.sprite(0, floor.height, 'player');
+    player = this.physics.add.sprite((60 + (door.width / 2)), (window.innerHeight * 0.7), 'player');
+    player.setOrigin(0, 0);
     player.setScale(3);
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
@@ -86,7 +109,7 @@ function update () {
         player.setVelocityX(120);
         player.anims.play('right', true)
     } else if (cursors.down.isDown) {
-        player.setVelocityY(200);
+        player.setVelocityY(400);
     } else {
         player.setVelocityX(0);
     }
