e24a025e57affa8a8296e045ffc841a504763316
Anthony O'Brien <aux.fuse@yahoo.com>
2021-08-07 21:23:45 +0100

    Added female player character spritesheet and tested animation of same

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,12 +18,14 @@ let config = {
 
 let game = new Phaser.Game(config);
 
+let playerSprite = 'assets/tileset/frames/playerSprite/wiz-m-spritesheet.png';
+
 function preload () {
     this.load.image('floor', 'assets/tileset/frames/environment/floor/floor-group.png')
 
     this.load.spritesheet(
         'player',
-        'assets/tileset/frames/playerSprite/wiz-m-spritesheet.png',
+        playerSprite,
         {
             frameWidth: 15,
             frameHeight: 21
@@ -34,7 +36,7 @@ function preload () {
 function create () {
     // create level platforms
     platforms = this.physics.add.staticGroup();
-    floor = platforms.create(0, ((window.innerHeight * 1) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor = platforms.create(0, ((window.innerHeight * 1) - 48) ,'floor').setScale(3).setOrigin(0, 0).refreshBody();
     floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.7) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
     floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.8) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
 
@@ -67,23 +69,29 @@ function create () {
         repeat: -1
     })
 
+    // add collision between player and platforms
     this.physics.add.collider(player, platforms);
+
+    // create keyboard detection for up/down/right/left arrow
+    cursors = this.input.keyboard.createCursorKeys();
+
 };
 
 function update () {
-    cursors = this.input.keyboard.createCursorKeys();
 
     if (cursors.left.isDown) {
-        player.setVelocityX(-100);
+        player.setVelocityX(-120);
         player.anims.play('left', true)
     } else if (cursors.right.isDown) {
-        player.setVelocityX(100);
+        player.setVelocityX(120);
         player.anims.play('right', true)
+    } else if (cursors.down.isDown) {
+        player.setVelocityY(200);
     } else {
         player.setVelocityX(0);
     }
 
     if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-300);
+        player.setVelocityY(-350);
     }
 };
