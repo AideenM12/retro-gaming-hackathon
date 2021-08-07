b9505b5c0479174fae3dd268c08eef3ae1b9598d
Anthony O'Brien <aux.fuse@yahoo.com>
2021-08-07 19:16:11 +0100

    Basic level with simple physics and spritesheet rendering to screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,13 @@ let config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
     height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -12,18 +19,44 @@ let config = {
 let game = new Phaser.Game(config);
 
 function preload () {
-    this.load.image('wall', 'assets/tileset/frames/wall_goo.png')
-    this.load.image('player', 'assets/tileset/frames/wizzard_m_hit_anim_f0.png');
+    this.load.image('floor', 'assets/tileset/frames/environment/floor/floor-group.png')
+
+    this.load.spritesheet(
+        'player',
+        'assets/tileset/frames/playerSprite/wiz-m-spritesheet.png',
+        {
+            frameWidth: 32,
+            frameHeight: 48
+        }
+    )
 };
 
 function create () {
-    sprite = this.add.image(400, 300, 'player');
-    sprite.setOrigin(0, 0);
-    sprite.setScale(1.5);
+    // create level platforms
+    platforms = this.physics.add.staticGroup();
+    floor = platforms.create(0, ((window.innerHeight * 1) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.7) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.8) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
 
-    wall = this.add.image(100, 100, 'wall');
-    wall.setOrigin(0, 0);
-    wall.setScale(3);
+    // character
+    player = this.physics.add.sprite(0, floor.height, 'player');
+    player.setScale(3);
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers(
+            'player', {
+                start: 0,
+                end: 3
+            }
+        ),
+        frameRate: 10,
+        repeat: -1
+    })
+
+    this.physics.add.collider(player, platforms);
 };
 
 function update () {
