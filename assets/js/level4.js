7f321503726a15fcff2a59ab8b2d282e66d19eec
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-08-09 09:40:41 +0000

    added level 4 js and html. positiond basic assets

--- assets/js/level4.js
+++ assets/js/level4.js
@@ -1,3 +1,4 @@
+// Instantiate Phaser
 let config = {
     type: Phaser.AUTO,
     width: window.innerWidth,
@@ -61,12 +62,16 @@ function create () {
 
     // create level platforms
     platforms = this.physics.add.staticGroup();
-    floor = platforms.create(0, ((window.innerHeight * 1) - 48) ,'floor').setScale(3).setOrigin(0, 0).refreshBody();
-    floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.7) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
-    floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.8) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
-    floor4 = platforms.create(((floor.width * 3) + (floor3.width * 6)), (window.innerHeight * 0.5), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
-    floor5 = platforms.create(((floor.width * 3.5) + (floor4.width)), (window.innerHeight * 0.2), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
-
+    floor = platforms.create(0, ((window.innerHeight * 1) - 280) ,'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor2 = platforms.create((floor.width * 3.2), ((window.innerHeight * 0.9) - 48), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor3 = platforms.create(((floor.width * 3) + (floor2.width * 4)), ((window.innerHeight * 0.7) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
+    floor4 = platforms.create(((floor.width * 3) + (floor3.width * 6)), (window.innerHeight *  0.9), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor5 = platforms.create(((floor.width * 5) + (floor4.width)), (window.innerHeight * 0.28), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor6 = platforms.create(((floor.width * 4) + (floor4.width)), (window.innerHeight * 0.28), 'floor').setScale(3).setOrigin(0, 0).refreshBody();
+    floor7 = platforms.create(((floor.width * 3) + (floor2.width * - 2)), ((window.innerHeight * 0.4) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
+    floor8 = platforms.create(((floor.width * 3) + (floor2.width * - 1)), ((window.innerHeight * 0.2) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
+    floor9 = platforms.create(((floor.width * 3) + (floor2.width *  0.5)), ((window.innerHeight * 0.2) - 48), 'floor').setScale(1.5, 3).setOrigin(0, 0).refreshBody();
+    
     // kill floor
     kill = this.physics.add.staticGroup();
     killFloor = kill.create((floor.width * 3), ((window.innerHeight * 1) - 48), 'acid-floor').setScale(4, 3).setOrigin(0, 0).refreshBody();
@@ -83,17 +88,17 @@ function create () {
     });
 
     // static scene objects doors/skulls/ladders etc
-    door = this.add.image(50, (window.innerHeight * 0.65), 'door').setScale(3).setOrigin(0, 0);
+    door = this.add.image(1270, (window.innerHeight * 0.48), 'door').setScale(3).setOrigin(0, 0);
     doorCloseAudio = this.sound.play('door-close');
 
-    column1 = this.add.image(((window.innerWidth / 2) - 240), (window.innerHeight * 0.16), 'column').setScale(3).setOrigin(0, 0);
-    columnRedBanner = this.add.image(((window.innerWidth / 2) - 240), ((window.innerHeight * 0.10) + 10), 'column-red-banner').setScale(3).setOrigin(0, 0);
-    column2 = this.add.image(((window.innerWidth / 2) - 10), (window.innerHeight * 0.16), 'column-flipped').setScale(3).setOrigin(0, 0);
-    columnRedBanner2 = this.add.image(((window.innerWidth / 2) - 10), ((window.innerHeight * 0.10) + 10), 'column-red-banner-flipped').setScale(3).setOrigin(0, 0);
+    column1 = this.add.image(((window.innerWidth / 1) - 308), (window.innerHeight * 0.21), 'column').setScale(3).setOrigin(0, 0);
+    columnRedBanner = this.add.image(((window.innerWidth / 1) - 308), ((window.innerHeight * 0.13) + 10), 'column-red-banner').setScale(3).setOrigin(0, 0);
+    column2 = this.add.image(((window.innerWidth / 1) - 70), (window.innerHeight * 0.21), 'column-flipped').setScale(3).setOrigin(0, 0);
+    columnRedBanner2 = this.add.image(((window.innerWidth / 1) - 70), ((window.innerHeight * 0.13) + 10), 'column-red-banner-flipped').setScale(3).setOrigin(0, 0);
 
     // exit door
     exit = this.physics.add.staticGroup();
-    exitDoor = exit.create(((window.innerWidth / 2) - 200), (window.innerHeight * 0.10), 'exit-door').setScale(3).setOrigin(0, 0).refreshBody();
+    exitDoor = exit.create(((window.innerWidth / 1) - 260), (window.innerHeight * 0.12), 'exit-door').setScale(3).setOrigin(0, 0).refreshBody();
     
     singleFloorTile = this.add.image(50, (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
     singleFloorTile2 = this.add.image((50 + (singleFloorTile.width * 4)), (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
@@ -110,7 +115,7 @@ function create () {
     skull = this.add.image(40, (window.innerHeight * 0.9), 'skull').setScale(4).setOrigin(0, 0);
 
     // character
-    player = this.physics.add.sprite((60 + (door.width / 2)), (window.innerHeight * 0.7), 'player');
+    player = this.physics.add.sprite((1270 + (door.width / 2)), (window.innerHeight * 0.5), 'player');
     player.setOrigin(0, 0);
     player.setScale(3);
     player.setBounce(0.2);
