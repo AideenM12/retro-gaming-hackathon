c74e9910beca3b840c5b35b76647d69577d78da8
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2021-08-09 09:51:02 +0000

    dont all i can to possition assits in level4 js

--- assets/js/level4.js
+++ assets/js/level4.js
@@ -25,12 +25,8 @@ playerSprite = localStorage.getItem("playerSprite");
 function preload () {
     this.load.image('floor', 'assets/tileset/frames/environment/floor/floor-group.png');
     this.load.image('acid-floor', 'assets/tileset/frames/environment/floor/acid-floor.png');
-    this.load.image('single-floor', 'assets/tileset/frames/environment/floor/floor_5.png');
     this.load.image('door', 'assets/tileset/frames/environment/door/doors_leaf_closed.png');
     this.load.image('exit-door', 'assets/tileset/frames/environment/door/doors_all.png');
-    this.load.image('crate', 'assets/tileset/frames/environment/objects/crate.png');
-    this.load.image('skull', 'assets/tileset/frames/enemies/skull.png');
-    this.load.image('blue-Flask-Big', 'assets/tileset/frames/environment/objects/flask_big_blue.png');
     this.load.image('column', 'assets/tileset/frames/environment/column/wall_column_mid.png');
     this.load.image('column-flipped', 'assets/tileset/frames/environment/column/wall_column_mid-flipped.png');
     this.load.image('column-red-banner', 'assets/tileset/frames/environment/wall/wall_banner_red.png');
@@ -100,19 +96,6 @@ function create () {
     exit = this.physics.add.staticGroup();
     exitDoor = exit.create(((window.innerWidth / 1) - 260), (window.innerHeight * 0.12), 'exit-door').setScale(3).setOrigin(0, 0).refreshBody();
     
-    singleFloorTile = this.add.image(50, (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
-    singleFloorTile2 = this.add.image((50 + (singleFloorTile.width * 4)), (window.innerHeight * 0.75), 'single-floor').setScale(2, 1).setOrigin(0, 0);
-
-    // using crate image as pseudo-ladder image
-    ladder = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.71)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
-    ladder2 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.75)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
-    ladder3 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.79)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
-    ladder4 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.83)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
-    ladder5 = this.add.image((62 + (door.width / 2)), (door.height + (window.innerHeight * 0.87)), 'crate').setScale(2.5, 2).setOrigin(0, 0);
-
-    // ambient background objects
-    blueFlaskBig = this.add.image(40, (window.innerHeight * 0.92), 'blue-Flask-Big').setScale(2).setOrigin(0, 0);
-    skull = this.add.image(40, (window.innerHeight * 0.9), 'skull').setScale(4).setOrigin(0, 0);
 
     // character
     player = this.physics.add.sprite((1270 + (door.width / 2)), (window.innerHeight * 0.5), 'player');
